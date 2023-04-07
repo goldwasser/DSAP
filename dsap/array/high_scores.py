8373cf09434c9671e21f568b74c966162a6819f2
Michael Goldwasser <michael.goldwasser@slu.edu>
2023-04-06 20:57:31 -0500

    changing score to new_score

--- dsap/array/high_scores.py
+++ dsap/array/high_scores.py
@@ -37,11 +37,11 @@ class Scoreboard:
 
     def add(self, entry):
         """Consider adding entry to high scores."""
-        score = entry.get_score()
+        new_score = entry.get_score()
 
         # Does new entry qualify as a high score?
-        # answer is yes if board not full or score is higher than last entry
-        good = self._n < len(self._board) or score > self._board[-1].get_score()
+        # answer is yes if board not full or new score is higher than last entry
+        good = self._n < len(self._board) or new_score > self._board[-1].get_score()
 
         if good:
             if self._n < len(self._board):          # no score drops from list
@@ -49,7 +49,7 @@ class Scoreboard:
 
             # shift lower scores rightward to make room for new entry
             j = self._n - 1
-            while j > 0 and self._board[j-1].get_score() < score:
+            while j > 0 and self._board[j-1].get_score() < new_score:
                 self._board[j] = self._board[j-1]   # shift entry from j-1 to j
                 j -= 1                              # and decrement j
             self._board[j] = entry                  # when done, add new entry
