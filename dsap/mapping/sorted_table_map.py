106e6dce268038a75617d912309137d57caf79dc
Michael Goldwasser <michael.goldwasser@slu.edu>
2023-04-26 09:16:40 -0500

    refactored with nonrecursive _find_index

--- dsap/mapping/sorted_table_map.py
+++ dsap/mapping/sorted_table_map.py
@@ -4,25 +4,26 @@ class SortedTableMap(MapBase):
     """Map implementation using a sorted table."""
 
     #----------------------------- nonpublic behaviors -----------------------------
-    def _find_index(self, k, low, high):
+    def _find_index(self, k):
         """Return index of the leftmost item with key greater than or equal to k.
 
-        Return high + 1 if no such item qualifies.
+        Return len(self) if no such item qualifies.
 
         That is, j will be returned such that:
-        all items of slice table[low:j] have key < k
-        all items of slice table[j:high+1] have key >= k
+        all items of slice table[ :j] have key < k
+        all items of slice table[j: ] have key >= k
         """
-        if high < low:
-            return high + 1                                          # no element qualifies
-        else:
+        low = 0
+        high = len(self._table) - 1
+        while low <= high:
             mid = (low + high) // 2
-            if k == self._table[mid]._key:
-                return mid                                           # found exact match
-            elif k < self._table[mid]._key:
-                return self._find_index(k, low, mid - 1)             # Note: may return mid
-            else:
-                return self._find_index(k, mid + 1, high)            # answer is right of mid
+            if k == self._table[mid]._key:                           # exact match found
+                return mid                                           #    at index mid
+            elif k < self._table[mid]._key:                          # target is smaller than _table[mid]
+                high = mid-1                                         #    so continue searching left of mid
+            else:                                                    # target is less than _table[mid]
+                low = mid + 1                                        #    so continue searching right of mid
+        return low                                                   # low = 1+high for unsuccessful search
 
     #----------------------------- public behaviors -----------------------------
     def __init__(self):
@@ -35,14 +36,14 @@ class SortedTableMap(MapBase):
 
     def __getitem__(self, k):
         """Return value associated with key k (raise KeyError if not found)."""
-        j = self._find_index(k, 0, len(self._table) - 1)
+        j = self._find_index(k)
         if j == len(self._table) or self._table[j]._key != k:
             raise KeyError('Key Error: ' + repr(k))
         return self._table[j]._value
 
     def __setitem__(self, k, v):
         """Assign value v to key k, overwriting existing value if present."""
-        j = self._find_index(k, 0, len(self._table) - 1)
+        j = self._find_index(k)
         if j < len(self._table) and self._table[j]._key == k:
             self._table[j]._value = v                                # reassign value
         else:
@@ -50,7 +51,7 @@ class SortedTableMap(MapBase):
 
     def __delitem__(self, k):
         """Remove item associated with key k (raise KeyError if not found)."""
-        j = self._find_index(k, 0, len(self._table) - 1)
+        j = self._find_index(k)
         if j == len(self._table) or self._table[j]._key != k:
             raise KeyError('Key Error: ' + repr(k))
         self._table.pop(j)                                           # delete item
@@ -84,7 +85,7 @@ class SortedTableMap(MapBase):
 
         Return None if there does not exist such a key.
         """
-        j = self._find_index(k, 0, len(self._table) - 1)             # j's key >= k
+        j = self._find_index(k)                                      # j's key >= k
         if j < len(self._table) and self._table[j]._key == k:
             return (self._table[j]._key, self._table[j]._value)      # exact match
         elif j > 0:
@@ -97,7 +98,7 @@ class SortedTableMap(MapBase):
 
         Return None if there does not exist such a key.
         """
-        j = self._find_index(k, 0, len(self._table) - 1)             # j's key >= k
+        j = self._find_index(k)                                      # j's key >= k
         if j < len(self._table):
             return (self._table[j]._key, self._table[j]._value)
         else:
@@ -108,7 +109,7 @@ class SortedTableMap(MapBase):
 
         Return None if there does not exist such a key.
         """
-        j = self._find_index(k, 0, len(self._table) - 1)             # j's key >= k
+        j = self._find_index(k)                                      # j's key >= k
         if j > 0:
             return (self._table[j-1]._key, self._table[j-1]._value)  # Note use of j-1
         else:
@@ -119,7 +120,7 @@ class SortedTableMap(MapBase):
 
         Return None if there does not exist such a key.
         """
-        j = self._find_index(k, 0, len(self._table) - 1)             # j's key >= k
+        j = self._find_index(k)                                      # j's key >= k
         if j < len(self._table) and self._table[j]._key == k:
             j += 1                                                   # advanced past match
         if j < len(self._table):
@@ -136,7 +137,7 @@ class SortedTableMap(MapBase):
         if start is None:
             j = 0
         else:
-            j = self._find_index(start, 0, len(self._table)-1)       # find first result
+            j = self._find_index(start)                              # find first result
         while j < len(self._table) and (stop is None or self._table[j]._key < stop):
             yield (self._table[j]._key, self._table[j]._value)
             j += 1
