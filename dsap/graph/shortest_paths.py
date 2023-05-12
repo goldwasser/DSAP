7a8695d201a893bf28d030cd1f14bfeccbee07bc
Michael Goldwasser <michael.goldwasser@slu.edu>
2023-05-11 21:30:53 -0500

    switching from d to D to align with book

--- dsap/graph/shortest_paths.py
+++ dsap/graph/shortest_paths.py
@@ -8,7 +8,7 @@ def shortest_path_distances(g, src):
 
     Return dictionary mapping each reachable vertex to its distance from src.
     """
-    d = {}                                             # d[v] is upper bound from s to v
+    D = {}                                             # D[v] is upper bound from s to v
     cloud = {}                                         # map reachable v to its d[v] value
     pq = AdaptableHeapPriorityQueue()                  # vertex v will have key d[v]
     pqlocator = {}                                     # map from vertex to its pq locator
@@ -17,38 +17,38 @@ def shortest_path_distances(g, src):
     # the source having distance 0 and all others having infinite distance
     for v in g.vertices():
         if v is src:
-            d[v] = 0
+            D[v] = 0
         else:
-            d[v] = float('inf')                        # syntax for positive infinity
-        pqlocator[v] = pq.add(d[v], v)                 # save locator for future updates
+            D[v] = float('inf')                        # syntax for positive infinity
+        pqlocator[v] = pq.add(D[v], v)                 # save locator for future updates
 
     while not pq.is_empty():
         key, u = pq.remove_min()
-        cloud[u] = key                                 # its correct d[u] value
+        cloud[u] = key                                 # its correct D[u] value
         del pqlocator[u]                               # u is no longer in pq
         for e in g.incident_edges(u):                  # outgoing edges (u,v)
             v = e.opposite(u)
             if v not in cloud:
                 # perform relaxation step on edge (u,v)
                 wgt = e.element()
-                if d[u] + wgt < d[v]:                  # better path to v?
-                    d[v] = d[u] + wgt                  # update the distance
-                    pq.update(pqlocator[v], d[v], v)   # update the pq entry
+                if D[u] + wgt < D[v]:                  # better path to v?
+                    D[v] = D[u] + wgt                  # update the distance
+                    pq.update(pqlocator[v], D[v], v)   # update the pq entry
 
     return cloud                                       # only includes reachable vertices
 
-def shortest_path_tree(g, s, d):
-    """Reconstruct shortest-path tree rooted at vertex s, given distance map d.
+def shortest_path_tree(g, s, D):
+    """Reconstruct shortest-path tree rooted at vertex s, given distance map D.
 
     Return tree as a map from each reachable vertex v (other than s) to the
     edge e=(u,v) that is used to reach v from its parent u in the tree.
     """
     tree = {}
-    for v in d:
+    for v in D:
         if v is not s:
             for e in g.incident_edges(v, False):       # consider INCOMING edges
                 u = e.opposite(v)
                 wgt = e.element()
-                if d[v] == d[u] + wgt:
+                if D[v] == D[u] + wgt:
                     tree[v] = e                        # edge e is used to reach v
     return tree
